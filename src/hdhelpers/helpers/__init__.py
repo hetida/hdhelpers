b1e4956afa321cfc94a40b2a35913f137ba5e7ad
jenny kupzigSCRIPT <jkupzig@fuseki.com>
2026-07-07 08:01:30 +0000

    fix circular import

--- src/hdhelpers/helpers/__init__.py
+++ src/hdhelpers/helpers/__init__.py
@@ -1,7 +1,7 @@
 """Collection of useful functions to ease some operations in hetida designer code."""
 
-from .locale import get_locale
-from .timezone_handling import modify_timezone
+from hdhelpers.helpers.locale import get_locale
+from hdhelpers.helpers.timezone_handling import modify_timezone
 
 __all__ = [
     "modify_timezone",
--- src/hdhelpers/plotting/__init__.py
+++ src/hdhelpers/plotting/__init__.py
@@ -1,8 +1,8 @@
 """Collection of useful functions to ease plotting."""
 
-from ..plotting.color import FUSEKI_COLOR_CYCLE, resolve_color
-from ..plotting.plotly_theme import set_agnostic_theme, theme_agnostic_template
-from ..plotting.timestamp_ticks import TICKFORMATSTOPS, set_dt_ticks
+from hdhelpers.plotting.color import FUSEKI_COLOR_CYCLE, resolve_color
+from hdhelpers.plotting.plotly_theme import set_agnostic_theme, theme_agnostic_template
+from hdhelpers.plotting.timestamp_ticks import TICKFORMATSTOPS, set_dt_ticks
 
 __all__ = [
     "resolve_color",
